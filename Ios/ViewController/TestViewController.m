7f5cf968cdc851e37c91065df94313631fff8589
Tiehui.Lu <dannytiehui@hotmail.com>
2013-03-25 12:02:35 +0800

    第一次提交代码到本地

---
+++ Ios/ViewController/TestViewController.m
@@ -0,0 +1,38 @@
+//
+//  TestViewController.m
+//  Ios
+//
+//  Created by Tiehui.Lu on 13-3-25.
+//  Copyright (c) 2013年 zhnews. All rights reserved.
+//
+
+#import "TestViewController.h"
+
+@interface TestViewController ()
+
+@end
+
+@implementation TestViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
